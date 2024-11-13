4f46107a26f352c13847aa1e7c0d85a157b2dc45
ivanloaiza <ivanloaiza58@gmail.com>
2024-11-12 22:16:23 -0600

    rama actualizada

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,16 @@ function App() {
       <div className="App flex">
         <Sidebar />
         <div className="flex-1 p-4">
-          
+          <Header />
+
           {/* Configuración de las rutas */}
           <Routes>
-
-            {/* Ruta para el Dashboard que incluye todos los componentes */}
-            <Route path="/Dashboard" element={<Dashboard token={token} />} />
-
             {/* Ruta para el Login */}
             <Route path="/" element={<Login onLoginSuccess={setToken} />} />
+
+            {/* Ruta para el Dashboard que incluye todos los componentes y rutas secundarias */}
+            <Route path="/dashboard" element={<Dashboard token={token} />} />
           </Routes>
-
-                    {/* Ruta para el login */}
-                    <Route path="/" element={<Login onLoginSuccess={setToken} />} />
-
-                    {/* Ruta para el Dashboard, que contiene las rutas secundarias */}
-                    <Route path="/dashboard/*" element={<Dashboard token={token} />} />
-                </Routes>
         </div>
       </div>
     </Router>
